0e4d3c02d2ce067745b93a954578e4ab29f43579
Kai <seu@das-labor.org>
2026-02-19 11:29:25 +0700

    add GIT_KEY_BASE64 to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# This script unlocks all git-crypt encrypted files using one of the provided
+# key sources. After unlocking, it executes the command given as arguments.
+#
+# GIT_KEY: binary key content
+# GIT_KEY_FILE: path to a file containing the binary key
+# GIT_KEY_BASE64: base64-encoded key content
+
 set -eo pipefail
 
 KEY_PATH=$(mktemp -t git-crypt-key-XXXXXX)
@@ -7,8 +15,10 @@ if [ -n "$GIT_KEY" ]; then
     echo "$GIT_KEY" > "$KEY_PATH"
 elif [ -f "$GIT_KEY_FILE" ]; then
     cp "$GIT_KEY_FILE" "$KEY_PATH"
+elif [ -n "$GIT_KEY_BASE64" ]; then
+    echo "$GIT_KEY_BASE64" | base64 -d > "$KEY_PATH"
 else
-    echo "No GIT_KEY or GIT_KEY_FILE found. Skipping unlock."
+    echo "No GIT_KEY, GIT_KEY_BASE64 GIT_KEY_FILE found. Skipping unlock."
     rm -f "$KEY_PATH"
     KEY_PATH=""
 fi
