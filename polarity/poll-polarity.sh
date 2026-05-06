c0bdd8d8c05855f67cba977fe6ff809e651b6690
Kai <seu@das-labor.org>
2026-05-06 12:54:51 +0800

    pd: retain old data, link latest

--- polarity/poll-polarity.sh
+++ polarity/poll-polarity.sh
@@ -3,14 +3,12 @@
 set -e
 set -o pipefail
 
-while true; do
-  RFC3339=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-  PARQUET_FILE="polarity-$RFC3339.parquet"
-  SAFE_PARQUET_FILE=$(echo "${PARQUET_FILE}" | tr ':' '_')
+RFC3339=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+PARQUET_DIR="polarity-$RFC3339-output"
+SAFE_PARQUET_DIR=$(echo "${PARQUET_DIR}" | tr ':' '_')
 
-  echo "Starting polarity data update at $RFC3339"
-  uv run update-polarity.py --output-dir "${SAFE_PARQUET_FILE}" --log-level DEBUG --log-file "update-polarity-$RFC3339.log"
+echo "Starting polarity data update at $RFC3339"
+uv run update-polarity.py --output-dir "${SAFE_PARQUET_DIR}" --log-level DEBUG --log-file "update-polarity-$RFC3339.log"
 
-  #echo "Loading polarity data into database from ${SAFE_PARQUET_FILE}"
-  #uv run load-database.py --input "${SAFE_PARQUET_FILE}"
-done
+ln -sf "${SAFE_PARQUET_DIR}" "latest-data"
+
