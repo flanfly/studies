e272de686e7d9f73af4d00f2790b713b915f6440
Kai <seu@das-labor.org>
2026-05-18 13:04:33 +0800

    pd: login script

--- polarity/poll-polarity.sh
+++ polarity/poll-polarity.sh
@@ -8,7 +8,20 @@ PARQUET_DIR="polarity-$RFC3339-output"
 SAFE_PARQUET_DIR=$(echo "${PARQUET_DIR}" | tr ':' '_')
 
 echo "Starting polarity data update at $RFC3339"
-uv run update-polarity.py --output-dir "${SAFE_PARQUET_DIR}" --log-level DEBUG --log-file "update-polarity-$RFC3339.log"
 
+if [ -s "IDTOKEN" ]; then
+  uv run update-polarity.py \
+    --output-dir "${SAFE_PARQUET_DIR}" \
+    --log-level DEBUG \
+    --log-file "update-polarity-$RFC3339.log" \
+    --idtoken "$(cat IDTOKEN)"
+else
+  uv run update-polarity.py \
+    --output-dir "${SAFE_PARQUET_DIR}" \
+    --log-level DEBUG \
+    --log-file "update-polarity-$RFC3339.log"
+fi
+
+rm latest-data
 ln -sf "${SAFE_PARQUET_DIR}" "latest-data"
 
