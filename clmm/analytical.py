cf2566cab105aef6e2d8177528abcdab299b0697
Kai <seu@das-labor.org>
2026-07-29 12:41:27 +0700

    clmm: fix analytical.py

--- clmm/analytical.py
+++ clmm/analytical.py
@@ -1,13 +1,13 @@
 import polars as pl
 
 from tqdm import tqdm
-from typing import Optional, Dict, Any, Literal, Tuple
 
 import uniswapv3.math as v3math
 from uniswapv3.emulator import Emulator
-from cli.token import parse_amount, composition_in_range
+from cli.token import composition_in_range
 
 from math import floor
+import datetime as dt
 
 import sys
 from dotenv import load_dotenv
@@ -35,7 +35,7 @@ def liquidity_for_value(
     return capital * v3math.Q96 // as_base(a0, a1, sqrt_price_x96)
 
 
-def main():
+def main() -> None:
     df = pl.read_parquet("data.parquet").sort("ts")
     start = df.filter(
         (pl.col("fee_growth0").is_not_null()) & (pl.col("fee_growth1").is_not_null())
@@ -49,38 +49,59 @@ def main():
     )
 
     # const parameters
-    rebalance_cost = 2 * 10**6  # in token0
-    harvest_cost = 2 * 10**6  # in token0
+    d0 = 6
+    d1 = 18
+    f0 = 10**d0
+    f01 = 10 ** (d1 - d0)
+
+    # approx. gas cost of burn+swap+mint in token, not incl. swap fee
+    rebalance_cost = 2 * f0
+
+    # approx. gas cost of collect+swap in token0, not incl. swap fee
+    harvest_cost = 2 * f0
+
+    # position shape
     tick_spacing = 10  # for 0.005%
-    fee_pips = 500
-    position_width = 5  # 100 bps
-    harvest_interval = dt.timedelta(week=1)
-    reposition_interval = dt.hours(hours=2)
-    initial_capital = 100 * 10**6  # in token0
+    position_width = 5  # half num ticks left and right of active tick, 100 bps
+
+    # min. time between harvest and reposition
+    harvest_interval = dt.timedelta(weeks=1)
+    reposition_interval = dt.timedelta(hours=2)
+
+    # economics
+    fee_tier = 500
+    initial_capital = 100 * f0  # in token0
 
     # running state
-    tick_lower, tick_upper = None, None
-    liquidity = None
-    last_rebalance = None
-    last_harvest = None
+    tick_lower: int | None = None
+    tick_upper: int | None = None
+    liquidity: int | None = None
+    last_reposition: dt.datetime | None = None
+    last_harvest: dt.datetime | None = None
     pending_fees = 0
     capital = initial_capital
 
     # eval metrics
     frag = []
 
-    l.info(f"start at {start['ts'].item()} with ${capital/10**6:.2f}")
+    l.info(f"start at {start['ts'].item()} with ${initial_capital/f0:.2f}")
 
-    for i, row in tqdm(enumerate(train.iter_rows(named=True)), total=train.height):
+    for row in tqdm(train.iter_rows(named=True), total=train.height):
+        # const state
+        now = row["ts"]
         sqrt_price_x96 = int(row["sqrt_price_x96"])
         tick = v3math.get_tick_at_sqrt_ratio(sqrt_price_x96)
         pos_value = capital
 
+        # timeouts
+        harvest_blocked = now - (last_harvest or now) < harvest_interval
+        reposition_blocked = now - (last_reposition or now) < reposition_interval
+
         # predicates
         active_position = tick_lower is not None and tick_upper is not None
         in_range = active_position and tick_lower <= tick < tick_upper
-        reposition = not active_position
-        harvest = dt.timedelta(seconds=last_harvest - i) >= harvest_interval
+        harvest = not harvest_blocked and pending_fees > 0
+        reposition = False
 
         # trace
         new_gas = 0
@@ -88,9 +109,10 @@ def main():
         lower = None
         upper = None
 
-        # collect fees
+        # add swap fees
         if in_range:
-            assert liquidity > 0
+            assert liquidity is not None and liquidity > 0
+
             fee0 = int(float(row["fee_growth0"] or 0.0) * liquidity)
             fee1 = int(float(row["fee_growth1"] or 0.0) * liquidity)
 
@@ -99,6 +121,9 @@ def main():
 
         # see if we should reposition
         elif active_position:
+            assert tick_lower is not None and tick_upper is not None
+            assert liquidity is not None
+
             theta, mu = row["theta"], row["mu"]
             lower = (v3math.get_sqrt_ratio_at_tick(tick_lower) / (2**96)) ** 2
             upper = (v3math.get_sqrt_ratio_at_tick(tick_upper) / (2**96)) ** 2
@@ -107,8 +132,8 @@ def main():
             if lower <= mu < upper:
                 # XXX do something with theta
                 pass
-            elif i - last_rebalance > 12 * 3600:
-                reposition = True
+            else:
+                reposition = not reposition_blocked
 
             pos0, pos1 = composition_in_range(
                 sqrt_price_x96, tick_lower, tick_upper, liquidity
@@ -116,27 +141,15 @@ def main():
             pos_value = as_base(pos0, pos1, sqrt_price_x96)
 
         # swap and reposition
-        if reposition:
+        if reposition or not active_position:
             # mark position to market
             if active_position:
-                # harvest fees
-                if harvest:
-                    new_gas += int(
-                        (0.5 * pending_fees * fee_pips / 1_000_000) + harvest_cost
-                    )
-                    capital += fees
-                    pending_fees = 0
-                    last_harvest = i
-
                 # add swap fees
                 new_gas += int(
-                    (0.5 * pos_value * fee_pips / 1_000_000) + rebalance_cost
+                    (0.5 * pos_value * fee_tier / 1_000_000) + rebalance_cost
                 )
 
                 capital -= new_gas
-                if capital < 10**6:
-                    l.info(f"broke at {row['ts']}")
-                    break
 
             tick = v3math.get_tick_at_sqrt_ratio(sqrt_price_x96)
 
@@ -155,14 +168,26 @@ def main():
                 sqrt_price_x96, tick_lower, tick_upper, capital
             )
 
-            num_rebalances += 1
-            last_rebalance = i
+            last_reposition = now
 
-        # update running state
+        # harvest pending fees
+        if harvest:
+            new_gas += int((0.5 * pending_fees * fee_tier / 1_000_000) + harvest_cost)
 
-        human_price = (10 ** (d1 - d0)) / ((sqrt_price_x96 / (2**96)) ** 2)
-        human_lower = (10 ** (d1 - d0)) / lower if lower is not None else None
-        human_upper = (10 ** (d1 - d0)) / upper if upper is not None else None
+            capital += pending_fees - new_gas
+            pending_fees = 0
+
+            last_harvest = now
+
+        # still gas in the tank?
+        if capital < f0:
+            l.info(f"broke at {row['ts']}")
+            break
+
+        # update metrics
+        human_price = f01 / ((sqrt_price_x96 / (2**96)) ** 2)
+        human_lower = f01 / lower if lower is not None else None
+        human_upper = f01 / upper if upper is not None else None
 
         frag.append(
             {
@@ -174,7 +199,8 @@ def main():
                 "pending_fees": pending_fees,
                 "position": pos_value,
                 # state
-                "reposition": reposition,
+                "did_reposition": reposition,
+                "did_harvest": harvest,
                 "is_active": active_position and in_range,
                 # position
                 "price": human_price,
@@ -182,6 +208,10 @@ def main():
                 "upper": human_upper,
             }
         )
+
+    if len(frag) == 0:
+        l.error("no metrics")
+        return
 
     res = (
         pl.DataFrame(frag)
@@ -195,23 +225,23 @@ def main():
         .sort("ts")
     )
 
-    # mark position to market
+    assert tick_lower is not None and tick_upper is not None
+    assert liquidity is not None
+
+    # XXX mark position to market
     # XXX add swap and gas fees
     # XXX retrieve pending_fees, add gas and swap fees
-    pos0, pos1 = composition_in_range(sqrt_price_x96, tick_lower, tick_upper, liquidity)
-    pos_value = as_base(pos0, pos1, sqrt_price_x96)
 
-    d = 10**6
     start = res.head(1)
     end = res.tail(1)
     days = (end["ts"] - start["ts"]).days()
-    pnl = (end["equity"] - start["equity"]).item() / d
+    pnl = (end["equity"] - start["equity"]).item() / f0
     active = end["utilization"].item() * 100
-    roi = pnl / (initial_capital / d) * 100
+    roi = pnl / (initial_capital / f0) * 100
     ann = ((1 + pnl) ** (365 / days) - 1) * 100
-    fees = end["fees"].item() / d
-    gas = end["gas"].item() / d
-    num_rp = end["num_repositions"].item() / d
+    fees = end["fees"].item() / f0
+    gas = end["gas"].item() / f0
+    num_rp = end["num_repositions"].item()
 
     l.info(
         f"${pnl:.2f} ({roi:.2f}% ROI, {ann:.2f}% pa) pnl, ${fees:.2f} fees earned, spend ${gas:.2f} gas for {num_rp} rebalances over {days}, {active:.2f}% utilized"
